44283f977098c8ca762ba4585d7dfc4760a2483c
sydney <sydneyblackburn10@gmail.com>
2026-02-09 00:36:37 -0800

    expose backends for load testing - added README

---
+++ nginx-examples/loadbalancing/README.md
@@ -0,0 +1,10 @@
+# Nginx Load Balancing Examples
+
+## Build & Run
+```sh
+# update build path for Nginx in docker-compose.yml to the algorithm you want to run
+$ docker-compose up -d --build
+
+# Run load test
+$ ./test-load.sh
+```
--- nginx-examples/loadbalancing/test-load.sh
+++ nginx-examples/loadbalancing/test-load.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo "Running load test..."
 for i in {1..100}; do
-  curl -s -o /dev/null -w "%{http_code} %{time_total}s\n" http://localhost:8080/ &
+  curl -s -o /dev/null -w "%{http_code} %{time_total}s\n" \
+   http://localhost:8080/ &
 done
 
 wait
 
 echo "checking backend distribution:"
+curl -s http://localhost:3000 | jq '.requests'
 curl -s http://localhost:3001 | jq '.requests'
 curl -s http://localhost:3002 | jq '.requests'
-curl -s http://localhost:3003 | jq '.requests'
